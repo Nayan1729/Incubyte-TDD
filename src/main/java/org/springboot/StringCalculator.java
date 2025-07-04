554f90826c4921d6108379808310a3c563f2dfa0
Nayan Thacker <nayanthacker28@gmail.com>
2025-07-04 21:42:52 +0530

    [Passed Test 1.3] Added a test case with two numbers.

--- src/main/java/org/springboot/StringCalculator.java
+++ src/main/java/org/springboot/StringCalculator.java
@@ -1,15 +1,20 @@
 package org.springboot;
 
-/**
- * Hello world!
- *
- */
+
 public class StringCalculator
 {
     public int add(String stringInputNums){
         if(stringInputNums.isEmpty()){
             return 0;
         }
-        return Integer.parseInt(stringInputNums);
+        String nums[] = stringInputNums.split(",");
+        int sum = 0;
+        if(nums.length == 1){
+            sum = Integer.parseInt(nums[0]);
+        }
+        if(nums.length == 2){
+            sum = Integer.parseInt(nums[0])+Integer.parseInt(nums[1]);
+        }
+        return sum;
     }
 }
