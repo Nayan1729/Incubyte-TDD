f1df1150c914d9464a7d58d5205a069174c67fa2
Nayan Thacker <nayanthacker28@gmail.com>
2025-07-04 19:32:59 +0530

    [Set up] Build maven project and setup JUnit.

---
+++ src/main/java/org/springboot/SpringCalculator.java
@@ -0,0 +1,13 @@
+package org.springboot;
+
+/**
+ * Hello world!
+ *
+ */
+public class SpringCalculator
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
